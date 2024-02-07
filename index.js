785ea3706ad8a7756e44ae14471ecffeee072afd
kai10107 <kaiaustin@icloud.com>
2024-02-08 10:21:30 +1100

    completes lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  addingEventListener();
+});
+
+
+
 function addingEventListener() {
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
